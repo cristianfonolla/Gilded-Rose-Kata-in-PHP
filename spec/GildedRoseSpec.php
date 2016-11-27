7700061018dcf2eacfcbbb0fd7506c2bdc40a808
cristianfonolla <cristian.fonolla@gmail.com>
2016-11-27 22:26:40 +0100

    sync

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace spec;
+namespace spec\App;
 
 
 use App\GildedRose;
@@ -11,6 +11,40 @@ class GildedRoseSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
+        $this->beConstructedWith('normal',10,5);
         $this->shouldHaveType(GildedRose::class);
+    }
+
+    function it_updates_normal_items_before_sell_date()
+    {
+        $this->beConstructedWith('normal',10,5);
+        $this->tick();
+        $this->quality->shouldBe(9);
+        $this->sellIn->shouldBe(4);
+    }
+
+    //limit case
+    function it_updates_normal_items_on_sell_date()
+    {
+        $this->beConstructedWith('normal',10,0);
+        $this->tick();
+        $this->quality->shouldBe(8);
+        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_normal_items_after_sell_date()
+    {
+        $this->beConstructedWith('normal',10,-5);
+        $this->tick();
+        $this->quality->shouldBe(8);
+        $this->sellIn->shouldBe(-6);
+    }
+
+    function it_updates_normal_items_with_a_quality_of_0()
+    {
+        $this->beConstructedWith('normal',0,5);
+        $this->tick();
+        $this->quality->shouldBe(0);
+        $this->sellIn->shouldBe(4);
     }
 }
